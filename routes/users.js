f6f18d7d28eeb0f781751f330250123b03de9e68
Chris Vithaya <Chrisvithaya@gmail.com>
2023-07-05 18:32:12 -0700

    express work

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+router.route('/:id').get('/', (req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+})
+
+router.get('/', (req, res) => {
+    res.send('User List')
+})
+
+router.get('/new', (req, res) => {
+    res.send('User New Form')
+})
+
+
+router.get('/:id', (req, res) => {
+    req.params.id
+    res.send(`Get User With ID ${req.params.id}`)
+})
+
+
+
+module.exports = router
+
+// Added by Chris 
